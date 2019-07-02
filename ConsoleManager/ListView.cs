c8217e93228c8334ec656b086da3044bce4e01d8
Stas Sherstiuk <mrcomeback666@gmail.com>
2019-07-03 00:46:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleManager/ListView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleManager
+{
+    class ListView
+    {
+        private int _prevSelecteIndex;
+        private int _selectedIndex;
+        private bool _wasPainted;
+        private List<FileSystemInfo> _items { get; set; }
+        private FileSystemInfo _selectedItem { get; set; }
+        private bool _focused { get; set; }
+
+        public ListView(List<FileSystemInfo> items)
+        {
+            _items = items;
+        }
+
+        public void Render()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                
+                if (_wasPainted && i != _selectedIndex && i != _prevSelecteIndex)
+                {
+                    continue;
+                }
+
+                _selectedItem = _items[i];
+                var savedForeground = Console.ForegroundColor;
+                var savedBackGround = Console.BackgroundColor;
+                if (i == _selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                Console.CursorLeft = 0;
+                Console.CursorTop = i;
+                Console.Write(_selectedItem.Name);
+
+                Console.ForegroundColor = savedForeground;
+                Console.BackgroundColor = savedBackGround;
+            }
+            _wasPainted = true;
+        }
+
+        public void Update(ConsoleKeyInfo key)
+        {
+            _prevSelecteIndex = _selectedIndex;
+            if (key.Key == ConsoleKey.UpArrow && _selectedIndex != 0) {
+                _selectedIndex--;
+            }   
+            else if (key.Key == ConsoleKey.DownArrow && _selectedIndex < _items.Count - 1)
+            {
+                _selectedIndex++;
+            }
+        }
+    }
+}
---
+++ ConsoleManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            var listView = new ListView(new DirectoryInfo("C:\\").GetFileSystemInfos().ToList());
+
+            while (true)
+            {
+                listView.Render();
+                var key = Console.ReadKey();
+                listView.Update(key);
+            }
+        }
+    }
+}
